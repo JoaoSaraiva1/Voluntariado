1f56b468c4cc6251180f6fbc06f626ca6eb45836
joaos <joaosaraiva.s.js@gmail.com>
2021-06-25 09:39:23 +0100

    Criado fragmento para mostrar a lista de instituicoes

--- app/src/main/java/com/example/voluntariado/ListaInstituicoesFragment.kt
+++ app/src/main/java/com/example/voluntariado/ListaInstituicoesFragment.kt
@@ -5,15 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.voluntariado.databinding.FragmentFirstBinding
+import com.example.voluntariado.databinding.FragmentListaInstituicoesBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class ListaInstituicoesFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentListaInstituicoesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentListaInstituicoesBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +32,8 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
     }
 
     override fun onDestroyView() {
@@ -42,3 +41,5 @@ class FirstFragment : Fragment() {
         _binding = null
     }
 }
+
+
