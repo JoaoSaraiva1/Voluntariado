e026a35c9c6b61798a583709b093997b39bb3a8b
joaos <joaosaraiva.s.js@gmail.com>
2021-07-14 15:31:20 +0100

    Adicionar o nome da tarefa à classe Instituicoes

--- app/src/main/java/com/example/voluntariado/TabelaInstituicoes.kt
+++ app/src/main/java/com/example/voluntariado/TabelaInstituicoes.kt
@@ -33,8 +33,47 @@ class TabelaInstituicoes(db: SQLiteDatabase) {
         having: String?,
         orderBy: String?
     ): Cursor? {
-        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
 
+        val ultimaColuna = columns.size - 1
+
+        var posColNomeTarefa = -1 // -1 indica que a coluna não foi pedida
+        for (i in 0..ultimaColuna) {
+            if (columns[i] == CAMPO_EXTERNO_NOME_TAREFA) {
+                posColNomeTarefa = i
+                break
+            }
+        }
+
+        if (posColNomeTarefa == -1) {
+            return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+        }
+
+        var colunas = ""
+        for (i in 0..ultimaColuna) {
+            if (i > 0) colunas += ","
+
+            colunas += if (i == posColNomeTarefa) {
+                "${TabelaTarefas.NOME_TABELA}.${TabelaTarefas.CAMPO_NOME} AS $CAMPO_EXTERNO_NOME_TAREFA"
+            } else {
+                "${NOME_TABELA}.${columns[i]}"
+            }
+        }
+
+        val tabelas =
+            "$NOME_TABELA INNER JOIN ${TabelaTarefas.NOME_TABELA} ON ${TabelaTarefas.NOME_TABELA}.${BaseColumns._ID}=$CAMPO_ID_TAREFAS"
+
+        var sql = "SELECT $colunas FROM $tabelas"
+
+        if (selection != null) sql += " WHERE $selection"
+
+        if (groupBy != null) {
+            sql += " GROUP BY $groupBy"
+            if (having != null) " HAVING $having"
+        }
+
+        if (orderBy != null) sql += " ORDER BY $orderBy"
+
+        return db.rawQuery(sql, selectionArgs)
     }
 
     companion object {
@@ -44,8 +83,9 @@ class TabelaInstituicoes(db: SQLiteDatabase) {
         const val CAMPO_TELEFONE = "telefone"
         const val CAMPO_MORADA = "morada"
         const val CAMPO_ID_TAREFAS = "id_tarefas"
+        const val CAMPO_EXTERNO_NOME_TAREFA = "nome_tarefa"
 
-        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, CAMPO_NOME,CAMPO_TELEFONE,CAMPO_MORADA,CAMPO_ID_TAREFAS)
+        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, CAMPO_NOME,CAMPO_TELEFONE,CAMPO_MORADA,CAMPO_ID_TAREFAS, CAMPO_EXTERNO_NOME_TAREFA)
     }
 }
 
