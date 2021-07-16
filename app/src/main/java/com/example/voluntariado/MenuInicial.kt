8efce16999a2a3acc6cdf60dcafb08a91392261d
joaos <joaosaraiva.s.js@gmail.com>
2021-07-16 16:23:08 +0100

    Implementação do botao Instituicoes e navegacao para o fragmento da lista

--- app/src/main/java/com/example/voluntariado/MenuInicial.kt
+++ app/src/main/java/com/example/voluntariado/MenuInicial.kt
@@ -5,55 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.voluntariado.databinding.FragmentListaInstitucoesBinding
+import com.example.voluntariado.databinding.FragmentMenuInicialBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MenuInicial.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MenuInicial : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentMenuInicialBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_inicial, container, false)
+        DadosApp.fragment = this
+        (activity as MainActivity).menuAtual = R.menu.menu_inicial
+
+        _binding = FragmentMenuInicialBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonVoluntarios.setOnClickListener {
+            //findNavController().navigate(R.id.action_FragmentInicio_to_FragmentPaciente)
+        }
+        binding.buttonTarefas.setOnClickListener {
+            //findNavController().navigate(R.id.action_FragmentInicio_to_FragmentLocal)
+        }
+        binding.buttonInstituicoes.setOnClickListener {
+            findNavController().navigate(R.id.action_menuInicial_to_Lista_Instituicoes_Fragment)
+        }
+        binding.buttonSair.setOnClickListener {
+
+        }
+
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuInicial.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuInicial().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
